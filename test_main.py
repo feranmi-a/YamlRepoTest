05df5d026b0b3b16d2f66c47d07faeaf6ef4d11d
Feranmi <Oige5@rrc.ca>
2025-09-08 17:53:24 -0500

    uncoment

--- test_main.py
+++ test_main.py
@@ -11,12 +11,12 @@ def test_bye_returns_none():
     assert greet() is None
 
 
-# def test_hello_prints_expected_text(capsys):
-#     hello()
-#     captured = capsys.readouterr()
-#     assert captured.out.strip() == "Hello, DevOps learner!"
+def test_hello_prints_expected_text(capsys):
+    hello()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "Hello, DevOps learner!"
 
-# def test_bye_prints_expected_text(capsys):
-#     bye()
-#     captured = capsys.readouterr()
-#     assert captured.out.strip() == "GoodMorning!"
+def test_bye_prints_expected_text(capsys):
+    bye()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "GoodMorning!"
